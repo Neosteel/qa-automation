16ae51a4fb4a5b64618cdc75f1ca36a1251dcaad
Neosteel <129478723+Neosteel@users.noreply.github.com>
2026-04-14 02:49:47 -0400

    add conftest fixture and refactor tests

--- tests/test_intro.py
+++ tests/test_intro.py
@@ -1,31 +1,30 @@
-from playwright.sync_api import sync_playwright, expect 
+from playwright.sync_api import expect
 
 
+def test_login_bank_url(browser_page):
+    browser_page.locator('[name="uid"]').fill("mngr658217")
+    browser_page.locator('[name="password"]').fill("ezaqupY")
+    browser_page.locator('[name="btnLogin"]').click()
+    expect(browser_page).to_have_url("https://demo.guru99.com/V4/manager/Managerhomepage.php")
 
 
-def test_login_bank_url():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
-        page.goto("https://demo.guru99.com/V4")
-        page.locator('[name="uid"]').fill("mngr658217")
-        page.locator('[name="password"]').fill("ezaqupY")
-        page.locator('[name="btnLogin"]').click()
-        expect(page).to_have_url("https://demo.guru99.com/V4/manager/Managerhomepage.php")
+def test_login_fail(browser_page):
+    '''login test fail'''
+    dialog_message = []
+    browser_page.on("dialog", lambda dialog: (dialog_message.append(dialog.message), dialog.dismiss()))
+    browser_page.locator('[name="uid"]').fill("mngr658217")
+    browser_page.locator('[name="password"]').fill("abcb")
+    browser_page.locator('[name="btnLogin"]').click()
+    browser_page.wait_for_timeout(2000)
+    assert dialog_message[0] == "User or Password is not valid"
 
 
-def test_login_fail():
-    '''login test fail ''' 
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
-        page.goto("https://demo.guru99.com/V4")
-        page.locator('[name="uid"]').fill("mngr658217")
-        page.locator('[name="password"]').fill("abcb")
-        ''' add alert listener '''
-        dialog_message = []
-        page.on("dialog", lambda dialog: (dialog_message.append(dialog.message), dialog.dismiss()))
-        '''click login button '''
-        page.locator('[name="btnLogin"]').click()
-        page.wait_for_timeout(2000)
-        assert dialog_message[0] == "User or Password is not valid"
+def test_empty_userid_password(browser_page):
+    '''empty fields test'''
+    dialog_message = []
+    browser_page.on("dialog", lambda dialog: (dialog_message.append(dialog.message), dialog.dismiss()))
+    browser_page.locator('[name="uid"]').fill("")
+    browser_page.locator('[name="password"]').fill("")
+    browser_page.locator('[name="btnLogin"]').click()
+    browser_page.wait_for_timeout(2000)
+    assert dialog_message[0] == "User or Password is not valid"
