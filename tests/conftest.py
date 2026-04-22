bba64ead0598e1577011d0ea03310a218c6a15bb
Neosteel <129478723+Neosteel@users.noreply.github.com>
2026-04-22 00:17:16 -0400

    add headless mode for CI and UI tests in pipeline

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
-import pytest 
+import pytest
+import os
 from playwright.sync_api import sync_playwright
 
 @pytest.fixture
 def browser_page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless = True)
+        is_ci = os.environ.get("CI") == "true"
+        browser = p.chromium.launch(headless=is_ci)
         page = browser.new_page()
         page.goto("https://demo.guru99.com/V4")
         yield page
