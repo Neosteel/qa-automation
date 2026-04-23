71cb534169324d135d8ad3ffa21b9790f7b3de1c
Neosteel <129478723+Neosteel@users.noreply.github.com>
2026-04-23 02:14:11 -0400

    add Allure reports and decorators

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,11 @@
 import requests
 import pytest
-
+import allure
+@allure.feature("API Testing")
+@allure.story("Get Post")
+@allure.title("Verify GET request returns status 200 and correct data")
 def test_get_post():
+
     response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
 
     assert response.status_code == 200
@@ -11,7 +15,9 @@ def test_get_post():
     assert data["id"] == 1
     assert data["userId"] == 1
 
-
+@allure.feature("API Testing")
+@allure.story("Create Post")
+@allure.title("Verify POST request creates new post with status 201")
 def test_create_post():
     response = requests.post("https://jsonplaceholder.typicode.com/posts" , json = { 
         "title": "My Test Post",
@@ -21,10 +27,13 @@ def test_create_post():
     })
     assert response.status_code == 201
     data = response.json()
-    data["title"] == "My Test Post"
-    data["body"] == "This is my test body"
-    data["userId"] == 1
+    assert data["title"] == "My Test Post"
+    assert data["body"] == "This is my test body"
+    assert data["userId"] == 1
 
+@allure.feature("API Testing")
+@allure.story("update post")
+@allure.title("Verify PUT request updates post with status 200")
 def test_update_post():
     response = requests.put(
         "https://jsonplaceholder.typicode.com/posts/1",
@@ -40,6 +49,9 @@ def test_update_post():
     assert data["title"] == "Updated Title"
     assert data["id"] == 1
 
+@allure.feature("API Testing")
+@allure.story("delete post")
+@allure.title("Verify DELETE request deletes post with status 200")
 def test_delete_post():
     response = requests.delete("https://jsonplaceholder.typicode.com/posts/1")
 
